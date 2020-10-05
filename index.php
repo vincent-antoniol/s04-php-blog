22fb848f8fc003b3c27452e6f6514515e571757d
Romain Tello <romain.tello@gmail.com>
2020-10-05 16:48:11 +0200

    Add culture category page

--- index.php
+++ index.php
@@ -27,22 +27,11 @@
 
     <ul style="padding-inline-start: 0px; display: grid; grid-template-columns: repeat(2, 1fr); gap: 1em;">
 
-        <?php foreach ($articles as $article): ?>
-        <li style="list-style-type: none;">
-            <div class="card"><img class="card-img-top"
-                    src="<?= $article['image'] ?>">
-                <h3 class="card-header"><?= $article['title'] ?></h3>
-                <div class="card-body">
-                    <p class="card-text"><?= $article['summary'] ?></p><a
-                        href="/articles/<?= $article['id'] ?>.php"><button
-                            type="button" class="btn btn-primary">Read more…</button></a>
-                </div>
-                <div class="card-footer"><a href="/category/sports"><span
-                            class="badge badge-secondary"><?= $article['category'] ?></span></a> <small class="text-muted">Published on
-                        <?= $article['date'] ?></small></div>
-            </div>
-        </li>
-        <?php endforeach; ?>
+        <?php
+            foreach ($articles as $article) {
+                include './templates/article-preview.tpl.php';
+            }
+        ?>
 
     </ul>
 </main>
---
+++ templates/article-preview.tpl.php
@@ -0,0 +1,14 @@
+<li style="list-style-type: none;">
+    <div class="card"><img class="card-img-top"
+            src="<?= $article['image'] ?>">
+        <h3 class="card-header"><?= $article['title'] ?></h3>
+        <div class="card-body">
+            <p class="card-text"><?= $article['summary'] ?></p><a
+                href="/articles/<?= $article['id'] ?>.php"><button
+                    type="button" class="btn btn-primary">Read more…</button></a>
+        </div>
+        <div class="card-footer"><a href="/category/sports"><span
+                    class="badge badge-secondary"><?= $article['category'] ?></span></a> <small class="text-muted">Published on
+                <?= $article['date'] ?></small></div>
+    </div>
+</li>
